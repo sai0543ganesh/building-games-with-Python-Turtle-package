6e3a1772e9ef0ac8ff00a216a6ebe5a611c903fc
sai0543ganesh <162001826+sai0543ganesh@users.noreply.github.com>
2024-03-07 19:51:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,45 +1,44 @@
 from turtle import Screen,Turtle
-from paddle import Paddle
-from ball import Ball
+from snake import Snake
+from food import Food
 from scoreboard import Scoreboard
 import time
-screen = Screen()
+
+screen= Screen()
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.setup(width=800,height=600)
-screen.title("Pong Game")
+screen.title("My Snake Game")
 screen.tracer(0)
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
-ball=Ball()
+snake =Snake()
+food= Food()
 scoreboard=Scoreboard()
-
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
-
-game_is_on = True
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+game_is_on=True
 while game_is_on:
-    time.sleep(ball.move_speed)
     screen.update()
-    ball.move()
-    #detecting collision and bounce
-    if ball.ycor()>280 or ball.ycor()<-280:
-        ball.bounce_y()
-    # detecting the collision with paddle
-    if ball.distance(r_paddle)<50 and ball.xcor()>320 or ball.distance(l_paddle)<50 and ball.xcor()< -320:
-        ball.bounce_x()
+    time.sleep(0.1)
 
+    snake.move()
+    #collidion with Food
+    if snake.head.distance(food)<15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    #detect collision with wall
+    if snake.head.xcor()> 295 or snake.head.xcor() < -295 or snake.head.ycor()>295 or snake.head.ycor() <-295:
+        game_is_on= False
+        scoreboard.game_over()
+    for segmen in snake.segments[4:]:
+        if segmen ==snake.head:
+            pass
+        elif snake.head.distance(segmen)<10:
+            game_is_on=False
+            scoreboard.game_over()
 
-    if ball.xcor()>380:
-        ball.reset_position()
-        scoreboard.l_point()
-    if ball.xcor()<-380:
-        ball.reset_position()
-        scoreboard.r_point()
-
-
-
+    
 screen.exitonclick()
